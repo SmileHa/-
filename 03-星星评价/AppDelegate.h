35971c113f6bc0ad8f2e52c44ab556a0d9b3da65
主播 <@qq.com>
2016-11-06 10:10:35 +0800

    Initial Commit

---
+++ 03-星星评价/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  03-星星评价
+//
+//  Created by apple on 16/11/6.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
